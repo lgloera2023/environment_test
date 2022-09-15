93ef99ea24ce8f648795a1650fe3a300d76e726b
Luke Loera <lgloera2023@gmail.com>
2022-09-15 14:39:04 -0500

    Add author attribute to book model and run RSPEC

--- db/schema.rb
+++ db/schema.rb
@@ -17,6 +17,7 @@ ActiveRecord::Schema.define(version: 2022_08_26_160831) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
+    t.string "author"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
